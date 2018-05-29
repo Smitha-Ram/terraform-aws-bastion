acffb3443565195dd958626fe5999a4102021111
Lance R. Vick <lance@lrvick.net>
2018-05-29 13:46:53 -0700

    : -> =

--- ec2.tf
+++ ec2.tf
@@ -36,7 +36,7 @@ EOF
 data "ignition_systemd_unit" "sshd-port" {
   name = "sshd.socket"
   dropin = [{
-    name: "10-sshd-port.conf"
+    name = "10-sshd-port.conf"
     content = <<EOF
 [Socket]
 ListenStream=
