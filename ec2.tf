f02583675c62a566befbd1e6d1042ad0e269a139
James Callahan <jamescallahan@bitgo.com>
2020-07-09 17:45:26 +1000

    Migrate to terraform 0.12

--- ec2.tf
+++ ec2.tf
@@ -1,66 +1,69 @@
 // Tested in us-west-2
 data "aws_ami" "flatcar" {
   most_recent = true
-  owners = ["075585003325"]
+  owners      = ["075585003325"]
   filter {
     name   = "architecture"
     values = ["x86_64"]
   }
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
   filter {
-    name = "name"
+    name   = "name"
     values = ["Flatcar-stable-*"]
   }
 }
 
 data "ignition_user" "tunnel" {
-  name = "tunnel"
-  home_dir = "/dev/shm"
+  name           = "tunnel"
+  home_dir       = "/dev/shm"
   no_create_home = true
-  shell = "/bin/false"
+  shell          = "/bin/false"
 }
 
 data "ignition_file" "sshd_config" {
-  path = "/etc/ssh/sshd_config"
+  path       = "/etc/ssh/sshd_config"
   filesystem = "root"
   content {
     content = <<EOF
-AllowUsers ${join(" ",var.allowed_users)}
+AllowUsers ${join(" ", var.allowed_users)}
 AuthenticationMethods publickey
 PermitRootLogin no
 AuthorizedKeysCommandUser nobody
 AuthorizedKeysCommand /etc/ssh/authorized_keys.sh %u %f
 EOF
+
   }
 }
 
 data "ignition_file" "sshd_authorized_keys" {
-  path = "/etc/ssh/authorized_keys.sh"
+  path       = "/etc/ssh/authorized_keys.sh"
   filesystem = "root"
-  mode = "493"
-  uid = "0"
-  gid = "0"
+  mode       = "493"
+  uid        = "0"
+  gid        = "0"
   content {
     content = <<EOF
 #!/bin/bash
 curl -sf "${aws_s3_bucket.ssh_public_keys.website_endpoint}/$${2/SHA256:/}"
 EOF
+
   }
 }
 
 data "ignition_systemd_unit" "sshd_port" {
   name = "sshd.socket"
-  dropin = [{
-    name = "10-sshd-port.conf"
+  dropin {
+    name    = "10-sshd-port.conf"
     content = <<EOF
 [Socket]
 ListenStream=
 ListenStream=${var.ssh_port}
 EOF
-  }]
+
+  }
 }
 
 data "ignition_systemd_unit" "mask_docker" {
@@ -75,16 +78,16 @@ data "ignition_systemd_unit" "mask_containerd" {
 
 data "ignition_config" "bastion" {
   systemd = [
-    "${data.ignition_systemd_unit.sshd_port.rendered}",
-    "${data.ignition_systemd_unit.mask_containerd.rendered}",
-    "${data.ignition_systemd_unit.mask_docker.rendered}",
+    data.ignition_systemd_unit.sshd_port.rendered,
+    data.ignition_systemd_unit.mask_containerd.rendered,
+    data.ignition_systemd_unit.mask_docker.rendered,
   ]
   users = [
-    "${data.ignition_user.tunnel.rendered}",
+    data.ignition_user.tunnel.rendered,
   ]
   files = [
-    "${data.ignition_file.sshd_config.rendered}",
-    "${data.ignition_file.sshd_authorized_keys.rendered}",
+    data.ignition_file.sshd_config.rendered,
+    data.ignition_file.sshd_authorized_keys.rendered,
   ]
 }
 
@@ -92,19 +95,19 @@ data "ignition_config" "bastion" {
 // Source: https://docs.flatcar-linux.org/os/booting-on-ec2/
 //
 resource "aws_launch_configuration" "bastion" {
-  image_id = "${data.aws_ami.flatcar.image_id}"
-  instance_type = "${var.instance_type}"
-  user_data = "${data.ignition_config.bastion.rendered}"
+  image_id          = data.aws_ami.flatcar.image_id
+  instance_type     = var.instance_type
+  user_data         = data.ignition_config.bastion.rendered
   enable_monitoring = true
   security_groups = [
-    "${aws_security_group.bastion.id}"
+    aws_security_group.bastion.id,
   ]
   root_block_device {
     volume_size = "8"
   }
-  iam_instance_profile = "${var.iam_instance_profile}"
-  associate_public_ip_address = "${var.associate_public_ip_address}"
-  key_name = "${var.key_name}"
+  iam_instance_profile        = var.iam_instance_profile
+  associate_public_ip_address = var.associate_public_ip_address
+  key_name                    = var.key_name
   lifecycle {
     create_before_destroy = true
   }
--- s3.tf
+++ s3.tf
@@ -1,36 +1,39 @@
 resource "aws_s3_bucket" "ssh_public_keys" {
   bucket_prefix = "ssh-keys-"
-  acl = "public-read"
+  acl           = "public-read"
   website {
     index_document = "index.html"
   }
 }
 
 resource "aws_s3_bucket_object" "ssh_public_keys" {
-  bucket = "${aws_s3_bucket.ssh_public_keys.bucket}"
-  key = "${
+  bucket = aws_s3_bucket.ssh_public_keys.bucket
+  key = replace(
     replace(
-      replace(
-        base64sha256(
-          base64decode(
-            element(
-              split(
-                " ",
-                file("${var.authorized_keys_directory}/${element(var.authorized_key_names,count.index)}.pub")
+      base64sha256(
+        base64decode(
+          element(
+            split(
+              " ",
+              file(
+                "${var.authorized_keys_directory}/${element(var.authorized_key_names, count.index)}.pub",
               ),
-              "1"
-            )
-          )
+            ),
+            "1",
+          ),
         ),
-        "=",
-        ""
       ),
+      "=",
+      "",
+    ),
     "/\\/\\//",
     "\\//",
-    )
-  }"
-  content = "${file("${var.authorized_keys_directory}/${element(var.authorized_key_names,count.index)}.pub")}"
-  count = "${length(var.authorized_key_names)}"
-  depends_on = ["aws_s3_bucket.ssh_public_keys"]
-  acl = "public-read"
+  )
+
+  content = file(
+    "${var.authorized_keys_directory}/${element(var.authorized_key_names, count.index)}.pub",
+  )
+  count      = length(var.authorized_key_names)
+  depends_on = [aws_s3_bucket.ssh_public_keys]
+  acl        = "public-read"
 }
