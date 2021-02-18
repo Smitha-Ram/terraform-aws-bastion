17eff54309bcce156ac29b50762426057e9e3640
James Callahan <jamescallahan@bitgo.com>
2021-02-18 16:54:40 +1100

    Add option to skip creating egress rule

--- sg.tf
+++ sg.tf
@@ -16,6 +16,8 @@ resource "aws_security_group_rule" "bastion_ssh_ingress" {
 }
 
 resource "aws_security_group_rule" "bastion_all_egress" {
+  count = var.create_egress_rule ? 1 : 0
+
   type              = "egress"
   from_port         = "0"
   to_port           = "65535"
